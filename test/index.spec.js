06a43a8d392f4fddc3820c28d5c597d9ed1bb9a2
mkozjak <kozjakm1@gmail.com>
2016-06-17 11:11:17 +0200

    test: start rabbitmq-server and microservice
    Signed-off-by: mkozjak <kozjakm1@gmail.com>

---
+++ test/index.spec.js
@@ -0,0 +1,48 @@
+"use strict"
+
+const exec = require('child_process').exec
+const rabbitmq = require("./rabbitmq-server")
+const spawn = require('child_process').spawn
+
+let app = null
+
+describe("# api test", function()
+{
+    this.timeout(3000)
+
+    before("start rabbitmq server", function(done)
+    {
+        rabbitmq.start.then(function()
+        {
+            done()
+        }, function(error)
+        {
+            done(error)
+        })
+    })
+
+    before("start microservice", function(done)
+    {
+        exec("ps x | fgrep log-server.js | fgrep -v 'grep' | wc -l | awk '{$1=$1};1'", function(error, out)
+        {
+            if (out != 0)
+                return done()
+
+            app = spawn('node', ['dist/index.js'])
+
+            done()
+        })
+    })
+
+    after("stop microservice", function(done)
+    {
+        if (app != null)
+        {
+            app.kill("SIGKILL")
+        }
+
+        done()
+    })
+
+    it("should say something", function() {})
+})
