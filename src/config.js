1d2c75554753e5062815b04cfe5128f597e6529f
mkozjak <kozjakm1@gmail.com>
2016-06-24 15:43:53 +0200

    config: add rethinkdb data
    Signed-off-by: mkozjak <kozjakm1@gmail.com>

--- src/config.js
+++ src/config.js
@@ -1,10 +1,19 @@
 'use strict'
 
 module.exports.service_bus = {
-    protocol: "amqp",
     type: "rabbitmq",
+    protocol: "amqp",
     hostname: "localhost",
     port: 5672,
     username: "guest",
     password: "guest"
+}
+
+module.exports.database = {
+    type: "rethinkdb",
+    name: "test",
+    hostname: "localhost",
+    port: 28015,
+    username: "admin",
+    password: ""
 }
