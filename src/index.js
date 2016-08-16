cf9592e147e561ca25972021c8f4acfcb066f884
Mario Kozjak <kozjakm1@gmail.com>
2016-08-16 15:31:40 +0200

    reorganize code
    Signed-off-by: Mario Kozjak <kozjakm1@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const application = require("./application")
+const application = require("./lib/application")
 const Promise = require("bluebird")
 
 require("babel-runtime/core-js/promise").default = Promise
